7c7ba191106d39b0bf5e7eb99d6deffb6f17db95
glaszczyk <info@grzegorzlaszczyk.pl>
2016-08-03 00:05:59 +0200

    Próba zastosowania BEM. Ale jeszcze dużo niedociągnięć

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+	return gulp.src('./sass/main.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(rename('style.css'))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task("default", ["sass"], function(){
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+
+	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./index.html', browserSync.reload);
+});
